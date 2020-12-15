ae3fbc07bd8189f8f03a4c6e5b3d2622b44a44ab
nandanchitale <nandanchitale@gmail.com>
2020-12-15 18:26:51 +0530

    Added Angular Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDisplayComponent } from './movie-display/movie-display.component';
+import { FlopVotesComponent } from './flop-votes/flop-votes.component';
+import { RentalCartComponent } from './rentalCart/rental-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDisplayComponent,
+    FlopVotesComponent,
+    RentalCartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/rentalCart/rental-cart.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flop-votes',
+  template: `
+    <p>
+      Rental Cart works !
+    </p>
+  `,
+  styles: []
+})
+
+export class RentalCartComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
